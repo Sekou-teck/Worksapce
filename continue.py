d7cabfb73b2ce33eeaae513953a9e5c65219c2be
Sekou-tech <cheickfofanas@yahoo.fr>
2020-04-28 20:09:18 +0200

    Resumé_fonctions

--- continue.py
+++ continue.py
@@ -6,12 +6,4 @@ for i in range(4):
     print("fin iteration", i)
 print("apres la boucle")
 
-# Boucles
-
-for i in range(10):
-    print("debut iteration", i)
-    print("bonjour")
-    if i == 2:
-        break
-    print("fin iteration", i)
-print("apres la boucle")
+# Boucle 

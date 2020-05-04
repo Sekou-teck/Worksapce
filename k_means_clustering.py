5cb8c62b1b331465f406c7a3d977d03d2be59857
Sekou-tech <cheickfofanas@yahoo.fr>
2020-05-04 03:10:45 +0200

    clustering_and_recommender_system

---
+++ k_means_clustering.py
@@ -0,0 +1,595 @@
+# A - K-Means Clustering
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from sklearn.datasets.samples_generator import make_blobs
+%matplotlib inline
+
+np.random.seed(0)
+
+X, y = make_blobs(n_samples=5000, centers=[[4,4], [-2, -1], [2, -3], [1, 1]], cluster_std=0.9)
+
+plt.scatter(X[:, 0], X[:, 1], marker='.')
+
+k_means = KMeans(init = "k-means++", n_clusters = 4, n_init = 12)
+
+k_means.fit(X)
+
+k_means_labels = k_means.labels_
+k_means_labels
+
+k_means_cluster_centers = k_means.cluster_centers_
+k_means_cluster_centers
+
+# Visual plot
+
+# Initialize the plot with the specified dimensions.
+fig = plt.figure(figsize=(6, 4))
+
+# Colors uses a color map, which will produce an array of colors based on
+# the number of labels there are. We use set(k_means_labels) to get the
+# unique labels.
+colors = plt.cm.Spectral(np.linspace(0, 1, len(set(k_means_labels))))
+
+# Create a plot
+ax = fig.add_subplot(1, 1, 1)
+
+# For loop that plots the data points and centroids.
+# k will range from 0-3, which will match the possible clusters that each
+# data point is in.
+for k, col in zip(range(len([[4, 4], [-2, -1], [2, -3], [1, 1]])), colors):
+    # Create a list of all data points, where the data poitns that are
+    # in the cluster (ex. cluster 0) are labeled as true, else they are
+    # labeled as false.
+    my_members = (k_means_labels == k)
+
+    # Define the centroid, or cluster center.
+    cluster_center = k_means_cluster_centers[k]
+
+    # Plots the datapoints with color col.
+    ax.plot(X[my_members, 0], X[my_members, 1], 'w', markerfacecolor=col, marker='.')
+
+    # Plots the centroids with specified color, but with a darker outline
+    ax.plot(cluster_center[0], cluster_center[1], 'o', markerfacecolor=col, markeredgecolor='k', markersize=6)
+
+# Title of the plot
+ax.set_title('KMeans')
+
+# Remove x-axis ticks
+ax.set_xticks(())
+
+# Remove y-axis ticks
+ax.set_yticks(())
+
+# Show the plot
+plt.show()
+ # Practice
+
+# write your code here
+
+k_means3 = KMeans(init = "k-means++", n_clusters = 3, n_init = 12)
+k_means3.fit(X)
+fig = plt.figure(figsize=(6, 4))
+colors = plt.cm.Spectral(np.linspace(0, 1, len(set(k_means3.labels_))))
+ax = fig.add_subplot(1, 1, 1)
+for k, col in zip(range(len(k_means3.cluster_centers_)), colors):
+    my_members = (k_means3.labels_ == k)
+    cluster_center = k_means3.cluster_centers_[k]
+    ax.plot(X[my_members, 0], X[my_members, 1], 'w', markerfacecolor=col, marker='.')
+    ax.plot(cluster_center[0], cluster_center[1], 'o', markerfacecolor=col,  markeredgecolor='k', markersize=6)
+plt.show()
+
+# Customers segmentation with K-Means
+# URL : !wget -O Cust_Segmentation.csv https://s3-api.us-geo.objectstorage.softlayer.net/cf-courses-data/CognitiveClass/ML0101ENv3/labs/Cust_Segmentation.csv
+# Loading data
+import pandas as pd
+cust_df = pd.read_csv("Cust_Segmentation.csv")
+cust_df.head()
+# Pre-processing
+df = cust_df.drop('Address', axis=1)
+df.head()
+
+from sklearn.preprocessing import StandardScaler
+X = df.values[:,1:]
+X = np.nan_to_num(X)
+Clus_dataSet = StandardScaler().fit_transform(X)
+Clus_dataSet
+
+# Modeling
+
+clusterNum = 3
+k_means = KMeans(init = "k-means++", n_clusters = clusterNum, n_init = 12)
+k_means.fit(X)
+labels = k_means.labels_
+print(labels)
+
+# Insights (perspectives)
+
+df["Clus_km"] = labels
+df.head(5)
+
+# checking Centroid values by averaging the features in each cluster
+df.groupby('Clus_km').mean()
+
+# distribution des consommateurs age/revenu
+
+area = np.pi * ( X[:, 1])**2
+plt.scatter(X[:, 0], X[:, 3], s=area, c=labels.astype(np.float), alpha=0.5)
+plt.xlabel('Age', fontsize=18)
+plt.ylabel('Income', fontsize=16)
+
+plt.show()
+
+# 3D plot
+
+from mpl_toolkits.mplot3d import Axes3D
+fig = plt.figure(1, figsize=(8, 6))
+plt.clf()
+ax = Axes3D(fig, rect=[0, 0, .95, 1], elev=48, azim=134)
+
+plt.cla()
+# plt.ylabel('Age', fontsize=18)
+# plt.xlabel('Income', fontsize=16)
+# plt.zlabel('Education', fontsize=16)
+ax.set_xlabel('Education')
+ax.set_ylabel('Age')
+ax.set_zlabel('Income')
+
+ax.scatter(X[:, 1], X[:, 0], X[:, 3], c= labels.astype(np.float))
+
+# B - Hierarchical clustering
+
+# Import Hierarchical Clustering Agglomerative
+
+import numpy as np
+import pandas as pd
+from scipy import ndimage
+from scipy.cluster import hierarchy
+from scipy.spatial import distance_matrix
+from matplotlib import pyplot as plt
+from sklearn import manifold, datasets
+from sklearn.cluster import AgglomerativeClustering
+from sklearn.datasets.samples_generator import make_blobs
+%matplotlib inline
+
+# generating random data et plot it
+
+X1, y1 = make_blobs(n_samples=50, centers=[[4,4], [-2, -1], [1, 1], [10,4]], cluster_std=0.9)
+
+plt.scatter(X1[:, 0], X1[:, 1], marker='o')
+
+# variable Agglom
+agglom = AgglomerativeClustering(n_clusters = 4, linkage = 'average')
+
+# the fitting model of the Agglom
+agglom = AgglomerativeClustering(n_clusters = 4, linkage = 'average')
+
+# Plot
+
+# Create a figure of size 6 inches by 4 inches.
+plt.figure(figsize=(6, 4))
+
+# These two lines of code are used to scale the data points down,
+# Or else the data points will be scattered very far apart.
+
+# Create a minimum and maximum range of X1.
+x_min, x_max = np.min(X1, axis=0), np.max(X1, axis=0)
+
+# Get the average distance for X1.
+X1 = (X1 - x_min) / (x_max - x_min)
+
+# This loop displays all of the datapoints.
+for i in range(X1.shape[0]):
+    # Replace the data points with their respective cluster value
+    # (ex. 0) and is color coded with a colormap (plt.cm.spectral)
+    plt.text(X1[i, 0], X1[i, 1], str(y1[i]),
+             color=plt.cm.nipy_spectral(agglom.labels_[i] / 10.),
+             fontdict={'weight': 'bold', 'size': 9})
+
+# Remove the x ticks, y ticks, x and y axis
+plt.xticks([])
+plt.yticks([])
+# plt.axis('off')
+
+
+# Display the plot of the original data before clustering
+plt.scatter(X1[:, 0], X1[:, 1], marker='.')
+# Display the plot
+plt.show()
+
+# Matrix
+
+dist_matrix = distance_matrix(X1,X1)
+print(dist_matrix)
+
+# Variable Z creating
+Z = hierarchy.linkage(dist_matrix, 'complete')
+
+# Plot Z
+
+dendro = hierarchy.dendrogram(Z)
+
+# Practice Average
+Z = hierarchy.linkage(dist_matrix, 'average')
+dendro = hierarchy.dendrogram(Z)
+
+# Exemple : Clustering on vehicle dataset
+# URL : !wget -O cars_clus.csv https://s3-api.us-geo.objectstorage.softlayer.net/cf-courses-data/CognitiveClass/ML0101ENv3/labs/cars_clus.csv
+
+# Read data
+filename = 'cars_clus.csv'
+
+#Read csv
+pdf = pd.read_csv(filename)
+print ("Shape of dataset: ", pdf.shape)
+
+pdf.head(5)
+
+# Data cleaning
+
+print ("Shape of dataset before cleaning: ", pdf.size)
+pdf[[ 'sales', 'resale', 'type', 'price', 'engine_s',
+       'horsepow', 'wheelbas', 'width', 'length', 'curb_wgt', 'fuel_cap',
+       'mpg', 'lnsales']] = pdf[['sales', 'resale', 'type', 'price', 'engine_s',
+       'horsepow', 'wheelbas', 'width', 'length', 'curb_wgt', 'fuel_cap',
+       'mpg', 'lnsales']].apply(pd.to_numeric, errors='coerce')
+pdf = pdf.dropna()
+pdf = pdf.reset_index(drop=True)
+print ("Shape of dataset after cleaning: ", pdf.size)
+pdf.head(5)
+
+# select feature
+featureset = pdf[['engine_s',  'horsepow', 'wheelbas', 'width', 'length', 'curb_wgt', 'fuel_cap', 'mpg']]
+
+# Normalization
+
+from sklearn.preprocessing import MinMaxScaler
+x = featureset.values #returns a numpy array
+min_max_scaler = MinMaxScaler()
+feature_mtx = min_max_scaler.fit_transform(x)
+feature_mtx [0:5]
+
+# Clustering using SciPy
+
+import scipy
+leng = feature_mtx.shape[0]
+D = scipy.zeros([leng,leng])
+for i in range(leng):
+    for j in range(leng):
+        D[i,j] = scipy.spatial.distance.euclidean(feature_mtx[i], feature_mtx[j])
+
+# Use complete
+
+import pylab
+import scipy.cluster.hierarchy
+Z = hierarchy.linkage(D, 'complete')
+
+from scipy.cluster.hierarchy import fcluster
+max_d = 3
+clusters = fcluster(Z, max_d, criterion='distance')
+clusters
+
+# Number of clusters
+
+from scipy.cluster.hierarchy import fcluster
+k = 5
+clusters = fcluster(Z, k, criterion='maxclust')
+clusters
+
+# Plot Dendrogram
+
+fig = pylab.figure(figsize=(18, 50))
+
+
+def llf(id):
+    return '[%s %s %s]' % (pdf['manufact'][id], pdf['model'][id], int(float(pdf['type'][id])))
+
+
+dendro = hierarchy.dendrogram(Z, leaf_label_func=llf, leaf_rotation=0, leaf_font_size=12, orientation='right')
+
+# Clustering using Scikit Learning
+
+dist_matrix = distance_matrix(feature_mtx,feature_mtx)
+print(dist_matrix)
+
+# Average Linkage
+
+agglom = AgglomerativeClustering(n_clusters = 6, linkage = 'complete')
+agglom.fit(feature_mtx)
+agglom.labels_
+
+# New field to show the clusters
+
+pdf['cluster_'] = agglom.labels_
+pdf.head()
+
+
+import matplotlib.cm as cm
+n_clusters = max(agglom.labels_)+1
+colors = cm.rainbow(np.linspace(0, 1, n_clusters))
+cluster_labels = list(range(0, n_clusters))
+
+# Create a figure of size 6 inches by 4 inches.
+plt.figure(figsize=(16,14))
+
+for color, label in zip(colors, cluster_labels):
+    subset = pdf[pdf.cluster_ == label]
+    for i in subset.index:
+            plt.text(subset.horsepow[i], subset.mpg[i],str(subset['model'][i]), rotation=25)
+    plt.scatter(subset.horsepow, subset.mpg, s= subset.price*10, c=color, label='cluster'+str(label),alpha=0.5)
+#    plt.scatter(subset.horsepow, subset.mpg)
+plt.legend()
+plt.title('Clusters')
+plt.xlabel('horsepow')
+plt.ylabel('mpg')
+
+# summarize the clusters
+
+pdf.groupby(['cluster_','type'])['cluster_'].count()
+
+# Characteristics of cars
+
+agg_cars = pdf.groupby(['cluster_','type'])['horsepow','engine_s','mpg','price'].mean()
+agg_cars
+
+# C - Density Based Clustering (DBSCAN Clustering)
+
+import numpy as np
+from sklearn.cluster import DBSCAN
+from sklearn.datasets.samples_generator import make_blobs
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+# Data generation
+def createDataPoints(centroidLocation, numSamples, clusterDeviation):
+    # Create random data and store in feature matrix X and response vector y.
+    X, y = make_blobs(n_samples=numSamples, centers=centroidLocation,
+                      cluster_std=clusterDeviation)
+
+    # Standardize features by removing the mean and scaling to unit variance
+    X = StandardScaler().fit_transform(X)
+    return X, y
+# Create Data points
+X, y = createDataPoints([[4,3], [2,-1], [-1,4]] , 1500, 0.5)
+ # Modeling
+
+epsilon = 0.3
+minimumSamples = 7
+db = DBSCAN(eps=epsilon, min_samples=minimumSamples).fit(X)
+labels = db.labels_
+labels
+
+# Distinguish outlier
+
+# Firts, create an array of booleans using the labels from db.
+core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
+core_samples_mask[db.core_sample_indices_] = True
+core_samples_mask
+
+# Number of clusters in labels, ignoring noise if present.
+n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+n_clusters_
+
+# Remove repetition in labels by turning it into a set.
+unique_labels = set(labels)
+unique_labels
+
+# Data Visualization
+
+# Create colors for the clusters.
+colors = plt.cm.Spectral(np.linspace(0, 1, len(unique_labels)))
+
+# Plot the points with colors
+for k, col in zip(unique_labels, colors):
+    if k == -1:
+        # Black used for noise.
+        col = 'k'
+
+    class_member_mask = (labels == k)
+
+    # Plot the datapoints that are clustered
+    xy = X[class_member_mask & core_samples_mask]
+    plt.scatter(xy[:, 0], xy[:, 1],s=50, c=[col], marker=u'o', alpha=0.5)
+
+    # Plot the outliers
+    xy = X[class_member_mask & ~core_samples_mask]
+    plt.scatter(xy[:, 0], xy[:, 1],s=50, c=[col], marker=u'o', alpha=0.5)
+
+    # Practice
+
+    from sklearn.cluster import KMeans
+
+    k = 3
+    k_means3 = KMeans(init="k-means++", n_clusters=k, n_init=12)
+    k_means3.fit(X)
+    fig = plt.figure(figsize=(6, 4))
+    ax = fig.add_subplot(1, 1, 1)
+    for k, col in zip(range(k), colors):
+        my_members = (k_means3.labels_ == k)
+        plt.scatter(X[my_members, 0], X[my_members, 1], c=col, marker=u'o', alpha=0.5)
+    plt.show()
+# Exemple : Weather Station Clustering using DBSCAN & scikit-learn
+
+# Downloading data : url = !wget -O weather-stations20140101-20141231.csv https://s3-api.us-geo.objectstorage.softlayer.net/cf-courses-data/CognitiveClass/ML0101ENv3/labs/weather-stations20140101-20141231.csv
+# Reading Data
+import csv
+import pandas as pd
+import numpy as np
+
+filename='weather-stations20140101-20141231.csv'
+
+#Read csv
+pdf = pd.read_csv(filename)
+pdf.head(5)
+
+# Cleaning
+
+pdf = pdf[pd.notnull(pdf["Tm"])]
+pdf = pdf.reset_index(drop=True)
+pdf.head(5)
+
+# Visualization
+
+from mpl_toolkits.basemap import Basemap
+import matplotlib.pyplot as plt
+from pylab import rcParams
+%matplotlib inline
+rcParams['figure.figsize'] = (14,10)
+
+llon=-140
+ulon=-50
+llat=40
+ulat=65
+
+pdf = pdf[(pdf['Long'] > llon) & (pdf['Long'] < ulon) & (pdf['Lat'] > llat) &(pdf['Lat'] < ulat)]
+
+my_map = Basemap(projection='merc',
+            resolution = 'l', area_thresh = 1000.0,
+            llcrnrlon=llon, llcrnrlat=llat, #min longitude (llcrnrlon) and latitude (llcrnrlat)
+            urcrnrlon=ulon, urcrnrlat=ulat) #max longitude (urcrnrlon) and latitude (urcrnrlat)
+
+my_map.drawcoastlines()
+my_map.drawcountries()
+# my_map.drawmapboundary()
+my_map.fillcontinents(color = 'white', alpha = 0.3)
+my_map.shadedrelief()
+
+# To collect data based on stations
+
+xs,ys = my_map(np.asarray(pdf.Long), np.asarray(pdf.Lat))
+pdf['xm']= xs.tolist()
+pdf['ym'] =ys.tolist()
+
+#Visualization1
+for index,row in pdf.iterrows():
+#   x,y = my_map(row.Long, row.Lat)
+   my_map.plot(row.xm, row.ym,markerfacecolor =([1,0,0]),  marker='o', markersize= 5, alpha = 0.75)
+#plt.text(x,y,stn)
+plt.show()
+
+# Clustering of station based on their location
+from sklearn.cluster import DBSCAN
+import sklearn.utils
+from sklearn.preprocessing import StandardScaler
+sklearn.utils.check_random_state(1000)
+Clus_dataSet = pdf[['xm','ym']]
+Clus_dataSet = np.nan_to_num(Clus_dataSet)
+Clus_dataSet = StandardScaler().fit_transform(Clus_dataSet)
+
+# Compute DBSCAN
+db = DBSCAN(eps=0.15, min_samples=10).fit(Clus_dataSet)
+core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
+core_samples_mask[db.core_sample_indices_] = True
+labels = db.labels_
+pdf["Clus_Db"]=labels
+
+realClusterNum=len(set(labels)) - (1 if -1 in labels else 0)
+clusterNum = len(set(labels))
+
+
+# A sample of clusters
+pdf[["Stn_Name","Tx","Tm","Clus_Db"]].head(5)
+
+# set label
+set(labels)
+
+# --> Visualization of clusters based on local
+
+from mpl_toolkits.basemap import Basemap
+import matplotlib.pyplot as plt
+from pylab import rcParams
+%matplotlib inline
+rcParams['figure.figsize'] = (14,10)
+
+my_map = Basemap(projection='merc',
+            resolution = 'l', area_thresh = 1000.0,
+            llcrnrlon=llon, llcrnrlat=llat, #min longitude (llcrnrlon) and latitude (llcrnrlat)
+            urcrnrlon=ulon, urcrnrlat=ulat) #max longitude (urcrnrlon) and latitude (urcrnrlat)
+
+my_map.drawcoastlines()
+my_map.drawcountries()
+#my_map.drawmapboundary()
+my_map.fillcontinents(color = 'white', alpha = 0.3)
+my_map.shadedrelief()
+
+# To create a color map
+colors = plt.get_cmap('jet')(np.linspace(0.0, 1.0, clusterNum))
+
+
+
+#Visualization1
+for clust_number in set(labels):
+    c=(([0.4,0.4,0.4]) if clust_number == -1 else colors[np.int(clust_number)])
+    clust_set = pdf[pdf.Clus_Db == clust_number]
+    my_map.scatter(clust_set.xm, clust_set.ym, color =c,  marker='o', s= 20, alpha = 0.85)
+    if clust_number != -1:
+        cenx=np.mean(clust_set.xm)
+        ceny=np.mean(clust_set.ym)
+        plt.text(cenx,ceny,str(clust_number), fontsize=25, color='red',)
+        print ("Cluster "+str(clust_number)+', Avg Temp: '+ str(np.mean(clust_set.Tm)))
+
+# Clustering with min max weather
+
+from sklearn.cluster import DBSCAN
+import sklearn.utils
+from sklearn.preprocessing import StandardScaler
+sklearn.utils.check_random_state(1000)
+Clus_dataSet = pdf[['xm','ym','Tx','Tm','Tn']]
+Clus_dataSet = np.nan_to_num(Clus_dataSet)
+Clus_dataSet = StandardScaler().fit_transform(Clus_dataSet)
+
+# Compute DBSCAN
+db = DBSCAN(eps=0.3, min_samples=10).fit(Clus_dataSet)
+core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
+core_samples_mask[db.core_sample_indices_] = True
+labels = db.labels_
+pdf["Clus_Db"]=labels
+
+realClusterNum=len(set(labels)) - (1 if -1 in labels else 0)
+clusterNum = len(set(labels))
+
+
+# A sample of clusters
+pdf[["Stn_Name","Tx","Tm","Clus_Db"]].head(5)
+
+# --> Visualization based on Temperature and local with 9 Clusters
+
+from mpl_toolkits.basemap import Basemap
+import matplotlib.pyplot as plt
+from pylab import rcParams
+%matplotlib inline
+rcParams['figure.figsize'] = (14,10)
+
+my_map = Basemap(projection='merc',
+            resolution = 'l', area_thresh = 1000.0,
+            llcrnrlon=llon, llcrnrlat=llat, #min longitude (llcrnrlon) and latitude (llcrnrlat)
+            urcrnrlon=ulon, urcrnrlat=ulat) #max longitude (urcrnrlon) and latitude (urcrnrlat)
+
+my_map.drawcoastlines()
+my_map.drawcountries()
+#my_map.drawmapboundary()
+my_map.fillcontinents(color = 'white', alpha = 0.3)
+my_map.shadedrelief()
+
+# To create a color map
+colors = plt.get_cmap('jet')(np.linspace(0.0, 1.0, clusterNum))
+
+
+
+#Visualization1
+for clust_number in set(labels):
+    c=(([0.4,0.4,0.4]) if clust_number == -1 else colors[np.int(clust_number)])
+    clust_set = pdf[pdf.Clus_Db == clust_number]
+    my_map.scatter(clust_set.xm, clust_set.ym, color =c,  marker='o', s= 20, alpha = 0.85)
+    if clust_number != -1:
+        cenx=np.mean(clust_set.xm)
+        ceny=np.mean(clust_set.ym)
+        plt.text(cenx,ceny,str(clust_number), fontsize=25, color='red',)
+        print ("Cluster "+str(clust_number)+', Avg Temp: '+ str(np.mean(clust_set.Tm)))
+
+
+
+
+
